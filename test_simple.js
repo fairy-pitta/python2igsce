6c014a4d0e5075fa8c1519ed072b9e35db595bea
Pitta <shuna120700@gmail.com>
2025-06-17 00:27:20 +0800

    Remove unnecessary files from root directory

--- test_simple.js
+++
@@ -1,7 +0,0 @@
-const { convertPython } = require('./dist/index.js');
-
-convertPython('x = 5').then(result => {
-  console.log('Result:', result);
-}).catch(err => {
-  console.error('Error:', err);
-});
